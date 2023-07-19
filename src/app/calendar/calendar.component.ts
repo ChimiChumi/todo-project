fff72be51b8ab9194384c5ffb1ed763a46997f22
Dobos Hunor <h044104@stud.u-szeged.hu>
2023-07-19 18:16:55 +0200

    Add calendar cell highlighting

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,70 +1,128 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ElementRef } from '@angular/core';
 import { DateAdapter } from '@angular/material/core';
 import { TodoWindowService } from '../services/todo-window.service';
-import { MatCalendarCellCssClasses } from '@angular/material/datepicker';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit{
+export class CalendarComponent implements OnInit, OnDestroy, AfterViewInit {
   selected: Date = new Date();
+  todoArray: any[] = [];
+
+  private subscription: Subscription = new Subscription();
+
 
   constructor(
     private adapter: DateAdapter<any>,
-    private todoWindow: TodoWindowService
+    private todoWindow: TodoWindowService,
+    private elementRef: ElementRef
   ) {
     this.adapter.setLocale('en-GB'); // 'en-GB' locale makes Monday the first day of the week
   }
 
   ngOnInit(): void {
     this.todoWindow.todos$.subscribe(todos => {
-      todos.forEach(element => {
-      });
+      this.todoArray = todos;
+      this.highlightCalendarCells();
     });
+    setTimeout(() => this.addNavigationListeners(), 0);
   }
 
-  isToday(): boolean {
+  ngAfterViewInit(): void {
+    this.highlightCalendarCells();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  isSameDate(date1: Date, date2: Date): boolean {
+    return date1.toDateString() === date2.toDateString();
+  }
+
+  isToday(date: Date): boolean {
     const today = new Date();
-
-    return this.selected?.getDate() === today.getDate() &&
-      this.selected?.getMonth() === today.getMonth() &&
-      this.selected?.getFullYear() === today.getFullYear();
+    return this.isSameDate(date, today);
   }
 
-  /**
-   * Transforms the standard date format into a more user-friendly result.
-   * @returns formatted, readable date
-   */
-  getFormattedDate(): string {
-    let year = this.selected.getFullYear();
-    // JavaScript months are 0-based, so +1 to get the correct month number
-    let month = ("0" + (this.selected.getMonth() + 1)).slice(-2);
-    let day = ("0" + this.selected.getDate()).slice(-2);
-
+  getFormattedDate(date: Date): string {
+    let year = date.getFullYear();
+    let month = ('0' + (date.getMonth() + 1)).slice(-2);
+    let day = ('0' + date.getDate()).slice(-2);
     return `${year}-${month}-${day}`;
   }
 
   /**
-   * This function displays the todo task if it matches the date.
-   * @returns a task happening on the selected date.
+   * Prints the task of the selected date in the card.
+   * @param date selected date
+   * @returns
    */
-  dailyTasks(): string {
-    let task = "";
+  dailyTasks(date: Date): string {
+    const formattedDate = this.getFormattedDate(date);
+    const tasks = this.todoArray
+      .filter(todo => todo.date === formattedDate)
+      .map(todo => todo.todo)
+      .join('\n');
+    return tasks;
+  }
 
-    this.todoWindow.todos$.subscribe(todos => {
-      todos.forEach(element => {
-        if (this.getFormattedDate() === element.date) {
-          if (task === "") {
-            task = element.todo;
-          }
-          else {
-            task += "\n" + element.todo;
-          }
+  /**
+   * Changes the appearance of the cells where a task is assigned.
+   */
+  highlightCalendarCells(): void {
+    const calendarCells = this.elementRef.nativeElement.querySelectorAll('.mat-calendar-body-cell');
+
+    calendarCells.forEach((cell: HTMLElement) => {
+      const ariaLabel = cell.getAttribute('aria-label');
+
+      if (ariaLabel) {
+        const date = new Date(ariaLabel);
+        const formattedDate = this.getFormattedDate(date);
+        const todoExists = this.todoArray.some(todo => todo.date === formattedDate);
+
+        // Reset styles for all cells first
+        cell.style.backgroundColor = '';
+        cell.style.boxShadow = '';
+        cell.style.borderRadius = '';
+
+        if (todoExists) {
+          cell.style.backgroundColor = 'transparent';
+          cell.style.boxShadow = 'inset 0 0 0 0.2rem #c195cf';
+          cell.style.borderRadius = '50%';
         }
+
+        if (this.isToday(date)) {
+          cell.style.backgroundColor = 'transparent';
+          cell.style.boxShadow = 'inset 0 0 0 0.2rem #e39f9f';
+          cell.style.borderRadius = '50%';
+        }
+      }
+    });
+  }
+
+  /**
+   * Workaround to highlight the cells which are not on the current calendar page.
+   */
+  addNavigationListeners(): void {
+    const prevButtons = this.elementRef.nativeElement.querySelectorAll('.mat-calendar-previous-button');
+    const nextButtons = this.elementRef.nativeElement.querySelectorAll('.mat-calendar-next-button');
+
+    prevButtons.forEach((button: { addEventListener: (arg0: string, arg1: () => void) => void; }) => {
+      button.addEventListener('click', () => {
+        setTimeout(() => this.highlightCalendarCells(), 0);
       });
     });
-    return task;
+
+    nextButtons.forEach((button: { addEventListener: (arg0: string, arg1: () => void) => void; }) => {
+      button.addEventListener('click', () => {
+        setTimeout(() => this.highlightCalendarCells(), 0);
+      });
+    });
   }
 }
