4e3ef254baa4db0a711fcd739d51f9bb576ceb72
Dobos Hunor <h044104@stud.u-szeged.hu>
2023-07-20 16:42:38 +0200

    Delete old calendar component

--- src/app/calendar/custom-date-adapter.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { NativeDateAdapter } from '@angular/material/core';
-
-@Injectable() // Added Injectable decorator
-export class CustomDateAdapter extends NativeDateAdapter {
-    override getFirstDayOfWeek(): number {
-        return 1; // 1 for Monday
-    }
-}
