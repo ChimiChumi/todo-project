af32d5956e52b8cabdd8d9e50d7f966b81afc27e
Dobos Hunor <h044104@stud.u-szeged.hu>
2023-07-22 16:15:12 +0200

    Fix bug when todo has no bullet in front

--- src/app/pipes/new-line.pipe.ts
+++ src/app/pipes/new-line.pipe.ts
@@ -7,13 +7,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 /**
  * This pipe automatically splits the string by '•',
  * manually creating a bullet-list appearance.
+ * To avoid invisible lines, it skips the first instance.
  */
 export class NewLinePipe implements PipeTransform {
   transform(value: string): string {
     if (value) {
-      const lines = value.split('•');
-      return lines.join('<br>•');
+      return value.replace(/\n/g, '<br>');
     }
-    return '';
+    return value || '';
   }
 }
